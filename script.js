b8f99e71e329834093ec746c1660ba89d191cf6a
Erin <146692565+erinmiso@users.noreply.github.com>
2025-03-03 22:29:45 -0800

    added data

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 const width = 500, height = 500;
 const sensitivity = 75;
 const initialScale = 250;
@@ -39,38 +38,51 @@ svg.call(d3.drag().on('drag', (event) => {
 let map = svg.append("g");
 let selectedCountries = new Set();
 
+
+// Load the world map
 d3.json("world.json").then(data => {
-    let countries = data.features;
+  let countries = data.features;
 
-    map.append("g")
-    .attr("class", "countries")
-    .selectAll("path")
-    .data(countries)
-    .enter().append("path")
-    .attr("class", d => "country_" + d.properties.name.replace(/ /g, "_"))
-    .attr("d", path)
-    .attr("fill", "white")
-    .style('stroke', 'black')
-    .style('stroke-width', 0.3)
-    .style("opacity", 0.8)
-        .on("click", (event, d) => {
-            let countryName = d.properties.name;
-            toggleCountry(countryName);
-        }) 
-        .on("mouseover", (event, d) => {
-          d3.select("#tooltip")
-              .style("visibility", "visible")
-              .style("left", `${event.pageX + 10}px`)
-              .style("top", `${event.pageY + 10}px`)
-              .text(d.properties.name);
-      })
-      .on("mouseout", () => {
-          d3.select("#tooltip").style("visibility", "hidden");
-      });
+  map.append("g")
+  .attr("class", "countries")
+  .selectAll("path")
+  .data(countries)
+  .enter().append("path")
+  .attr("class", d => "country_" + d.properties.name.replace(/ /g, "_"))
+  .attr("d", path)
+  .attr("fill", "white")
+  .style('stroke', 'black')
+  .style('stroke-width', 0.3)
+  .style("opacity", 0.8)
+  .on("click", (event, d) => {
+      let countryName = d.properties.name;
+      toggleCountry(countryName);
+  })
+  .on("mouseover", (event, d) => {
+      d3.select("#tooltip")
+          .style("visibility", "visible")
+          .style("left", `${event.pageX + 10}px`)
+          .style("top", `${event.pageY + 10}px`)
+          .text(d.properties.name);
+  })
+  .on("mouseout", () => {
+      d3.select("#tooltip").style("visibility", "hidden");
+  });
+});
+// Load health, military, and education data
+let healthData, militaryData, educationData;
 
-    
+Promise.all([
+    d3.json("cleaned_healthSpendinga.json"),
+    d3.json("cleaned_milSpendinga.json"),
+    d3.json("cleaned_educSpendinga.json")
+]).then(([health, military, education]) => {
+    healthData = health;
+    militaryData = military;
+    educationData = education;
 });
 
+// Stacked Bar Chart Setup
 const chartWidth = 600, chartHeight = 400;
 const chartSvg = d3.select("#chart")
     .append("svg")
@@ -88,18 +100,32 @@ const colorScale = d3.scaleOrdinal(d3.schemeCategory10);
 chartSvg.append("g").attr("class", "x-axis").attr("transform", "translate(0,300)");
 chartSvg.append("g").attr("class", "y-axis");
 
-/** Toggle Country and Update Stacked Bar Chart **/
+// Tooltip for displaying stack value
+const tooltip = d3.select("body").append("div")
+    .attr("class", "tooltip")
+    .style("position", "absolute")
+    .style("background-color", "rgba(0, 0, 0, 0.7)")
+    .style("color", "white")
+    .style("padding", "5px")
+    .style("border-radius", "3px")
+    .style("visibility", "hidden")
+    .text("Tooltip");
+
 function toggleCountry(country) {
     if ([...selectedCountries].some(d => d.name === country)) {
         selectedCountries = new Set([...selectedCountries].filter(d => d.name !== country));
     } else {
+        // Fetch data from the three JSON files
+        let healthExpenditure = getDataForCountry(healthData, country, "Domestic general government health expenditure (GGHE-D) as percentage of general government expenditure (GGE) (%)");
+        let militaryExpenditure = getDataForCountry(militaryData, country, "Military expenditure (% of government spending)");
+        let educationExpenditure = getDataForCountry(educationData, country, "Government expenditure on education, total (% of government expenditure)");
+
         let newData = {
             name: country,
             data: {
-                "Education": Math.random() * 100,
-                "Healthcare": Math.random() * 100,
-                "Defense": Math.random() * 100,
-                "Infrastructure": Math.random() * 100
+                "Health": healthExpenditure || 0,  // Default to 0 if data is missing
+                "Military": militaryExpenditure || 0,
+                "Education": educationExpenditure || 0
             }
         };
         selectedCountries.add(newData);
@@ -107,7 +133,13 @@ function toggleCountry(country) {
     updateChart();
 }
 
-/** Update Stacked Bar Chart **/
+// Function to fetch the correct data from each dataset
+function getDataForCountry(dataset, country, key) {
+    const countryData = dataset.find(d => d.Entity === country);
+    return countryData ? countryData[key] : null;
+}
+
+// Update Stacked Bar Chart
 function updateChart() {
     const dataArray = Array.from(selectedCountries);
     
@@ -116,7 +148,7 @@ function updateChart() {
         return;
     }
 
-    const spendingAreas = ["Education", "Healthcare", "Defense", "Infrastructure"];
+    const spendingAreas = ["Health", "Military", "Education"];
     
     xScale.domain(dataArray.map(d => d.name));
     yScale.domain([0, d3.max(dataArray, d => 
@@ -145,7 +177,42 @@ function updateChart() {
         .attr("x", d => xScale(d.data.name))
         .attr("y", d => yScale(d[1]))
         .attr("height", d => yScale(d[0]) - yScale(d[1]))
-        .attr("width", xScale.bandwidth());
+        .attr("width", xScale.bandwidth())
+        .on("mouseover", (event, d) => {
+            tooltip.style("visibility", "visible")
+                .style("left", `${event.pageX + 10}px`)
+                .style("top", `${event.pageY + 10}px`)
+                .text(`${d.data.name} - ${d.key}: ${d[1] - d[0]}%`);
+        })
+        .on("mouseout", () => {
+            tooltip.style("visibility", "hidden");
+        });
 
     bars.exit().remove();
 }
+
+// Create a legend for the stacked bar chart
+function createLegend() {
+    const legend = d3.select("#legend").append("svg")
+        .attr("width", 200)
+        .attr("height", 100)
+        .selectAll("g")
+        .data(["Health", "Military", "Education"])
+        .enter()
+        .append("g")
+        .attr("transform", (d, i) => `translate(0,${i * 20})`);
+
+    legend.append("rect")
+        .attr("width", 18)
+        .attr("height", 18)
+        .attr("fill", d => colorScale(d));
+
+    legend.append("text")
+        .attr("x", 25)
+        .attr("y", 9)
+        .attr("dy", ".35em")
+        .text(d => d);
+}
+
+// Initialize legend
+createLegend();
